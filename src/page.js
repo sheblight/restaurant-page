7ea28a045cfd6a5a5aa6b0b01e9463f79b13d682
sheblight <sheblight@gmail.com>
2023-01-19 15:52:30 -0500

    Add tab buttons to content

--- src/page.js
+++ src/page.js
@@ -2,7 +2,7 @@ const page = (()=> {
     const load = function() {
         const content = document.querySelector('div#content');
         const header = document.createElement("header");
-        const about = document.createElement("div");
+        const tabs = document.createElement("div");
 
         // set up header
         const headerBg = document.createElement("div");
@@ -11,8 +11,21 @@ const page = (()=> {
         headerBg.appendChild(headerText);
         header.appendChild(headerBg);
 
+        // setup tabs
+        const aboutBtn = document.createElement("button");
+        const menuBtn = document.createElement("button");
+        const contactBtn = document.createElement("button");
+        tabs.classList.add("tabs")
+        aboutBtn.textContent = "About";
+        menuBtn.textContent = "Menu";
+        contactBtn.textContent = "Contact";
+        tabs.appendChild(aboutBtn);
+        tabs.appendChild(menuBtn);
+        tabs.appendChild(contactBtn);
+
         // append to content
         content.appendChild(header);
+        content.appendChild(tabs);
         console.log("finish render");
     };
     return { load };
