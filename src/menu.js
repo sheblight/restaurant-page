ce558be6ef9f1c5a3335bda9cd77d47e3510a9af
sheblight <sheblight@gmail.com>
2023-01-19 17:39:55 -0500

    Add placeholder menu items

--- src/menu.js
+++ src/menu.js
@@ -1,4 +1,25 @@
+import Swamp from "./images/swamp.jpg";
+
 const menu = (()=> {
+    const createMenuItem = function(img, title, ingredients) {
+        const div = document.createElement("div");
+        const thumbnail = new Image();
+        const titleTxt = document.createElement("h2");
+        const ingredientTxt = document.createElement("p");
+
+        // set text and image
+        thumbnail.src = img;
+        titleTxt.textContent = title;
+        ingredientTxt.textContent = ingredients; 
+
+        div.classList.add("menu-item");
+        div.appendChild(thumbnail);
+        div.appendChild(titleTxt);
+        div.appendChild(ingredientTxt);
+
+        return div;
+    }
+
     const load = function() {
         const content = document.querySelector('div#content');
         const menuDiv = document.createElement('main');
@@ -8,6 +29,10 @@ const menu = (()=> {
         const headTxt = document.createElement("h1");
         headTxt.textContent = "Browse our options!"
         menuDiv.appendChild(headTxt);
+
+        // menu items
+        menuDiv.appendChild(createMenuItem(Swamp, "Beef", "a cow"));
+        menuDiv.appendChild(createMenuItem(Swamp, "Fun Dip", "bear"));
 
         // append to content
         content.appendChild(menuDiv);
