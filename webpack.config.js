c1414711c65bf8bd7dee68fdf19a3be37f66917c
sheblight <sheblight@gmail.com>
2023-01-19 11:01:20 -0500

    Add webpack server

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,9 @@ const path = require('path');
 module.exports = {
     mode: 'none',
     entry: './src/index.js',
+    devServer: {
+        static: './dist',
+    },
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
