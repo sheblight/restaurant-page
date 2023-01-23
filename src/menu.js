d1138e234f4f9019a6ba2a23701f82c3cdaa91ed
sheblight <sheblight@gmail.com>
2023-01-23 12:38:18 -0500

    Update menu items and update with unique icons

--- src/menu.js
+++ src/menu.js
@@ -1,16 +1,37 @@
 import Swamp from "./images/swamp.jpg";
+import Tire from "./images/tire.jpg";
+import Goop from "./images/goop.jpg";
+import Chicken from "./images/chicken.jpg";
 
 const menu = (()=> {
-    const createMenuItem = function(img, title, ingredients) {
+    function IconInfo(img, style) {
+        this.img = img;
+        this.style = style;
+    };
+    
+    function ItemInfo(title, ingredients, price) {
+        this.title = title;
+        this.ingredients = ingredients;
+        this.price = price; 
+    }
+
+    const menuItems = [
+        {icon:new IconInfo(Tire,"background-size:150%;"), item:new ItemInfo("Tire", "A chewy starter for everybody to enjoy!", 15)},
+        {icon:new IconInfo(Goop,"background-size:contain;"), item:new ItemInfo("Goop", "Skipped on your greens? Try these!", 6)},
+        {icon:new IconInfo(Chicken,"background-size:150%;"), item:new ItemInfo("Chicken", "", 40)}
+    ]
+
+    const createMenuItem = function(icon, item) {
         const div = document.createElement("div");
         const thumbnail = new Image();
         const titleTxt = document.createElement("h2");
         const ingredientTxt = document.createElement("p");
 
         // set text and image
-        thumbnail.src = img;
-        titleTxt.textContent = title;
-        ingredientTxt.textContent = ingredients; 
+        thumbnail.src = icon.img;
+        thumbnail.style = icon.style;
+        titleTxt.textContent = item.title;
+        ingredientTxt.textContent = item.ingredients; 
 
         div.classList.add("menu-item");
         div.appendChild(thumbnail);
@@ -31,8 +52,9 @@ const menu = (()=> {
         menuDiv.appendChild(headTxt);
 
         // menu items
-        menuDiv.appendChild(createMenuItem(Swamp, "Beef", "a cow"));
-        menuDiv.appendChild(createMenuItem(Swamp, "Fun Dip", "bear"));
+        menuItems.forEach(menuItem => {
+            menuDiv.appendChild(createMenuItem(menuItem.icon, menuItem.item));
+        });
 
         // append to content
         content.appendChild(menuDiv);
